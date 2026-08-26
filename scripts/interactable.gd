823ed4868d3dd6b6590190a73065b44c87b0c0d7
cdurran9 <carter.durrant@gmail.com>
2026-08-25 20:26:23 -0600

    Encapsulated interactions

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,18 +1,25 @@
-extends Node
-class_name Interactable
+class_name Interactable extends Node3D
 
 signal on_interact
 signal is_highlighted
 
+var text_visible := false
+
 @export var interact_text := "Interact"
-@export var interact_text_node : Label3D
+
+@onready var interact_text_node : Label3D = $InteractText
 
 func _ready():
 	if interact_text_node != null:
 		interact_text_node.text = interact_text
+		
+func _process(_delta):
+	interact_text_node.visible = text_visible
+	text_visible = false
 
 func interact():
 	on_interact.emit()
 
 func highlight():
+	text_visible = true
 	is_highlighted.emit()
--- scripts/interactable_test.gd
+++ scripts/interactable_test.gd
@@ -5,7 +5,6 @@ var highlight_width := 0
 
 @onready var interactable: Interactable = $Interactable
 @onready var mesh = $MeshInstance3D
-@onready var interact_text = $InteractText
 
 func _ready():
 	interactable.on_interact.connect(handle_interact)
@@ -14,7 +13,6 @@ func _ready():
 	
 func _process(_delta):
 	mat.next_pass.set("shader_parameter/outline_width", highlight_width)
-	interact_text.visible = true if highlight_width == 10 else false
 	if highlight_width == 0: return
 	highlight_width = 0
 	
